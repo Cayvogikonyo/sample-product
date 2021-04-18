3b6971d7a3ce47c00a4a92915f84714ecd688122
kelvin.gikonyo <kevohm.km@gmail.com>
2021-04-18 21:15:52 +0300

    opened routed

--- routes/api.php
+++ routes/api.php
@@ -24,18 +24,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::middleware(['auth:sanctum'])->group(function () {
 
-
     Route::get('elements', [OrderController::class, 'stats'])->name('elements');
     Route::get('orders', [OrderController::class, 'index'])->name('orders');
     Route::get('top-sales', [OrderController::class, 'topThree'])->name('top-sales');
     Route::get('products', [ProductController::class, 'index'])->name('products');
     Route::get('suppliers', [SupplierController::class, 'index'])->name('suppliers');
-    Route::get('product-supplies', [SupplierController::class, 'supplyProducts'])->name('product-supplies');
+    Route::get('product-suppliers', [SupplierController::class, 'supplyProducts'])->name('product-suppliers');
     Route::post('new-product', [ProductController::class, 'saveProduct'])->name('new-product');
     Route::post('new-supplier', [SupplierController::class, 'saveSupplier'])->name('new-supplier');
     Route::post('new-order', [OrderController::class, 'saveOrder'])->name('new-order');
     Route::post('delete-order', [OrderController::class, 'delete'])->name('delete-order');
     Route::get('reports/product-sales', [OrderController::class, 'orderStats'])->name('product-reports');
-    Route::post('new-supply', [SupplierController::class, 'saveSupplierProduct'])->name('new-supply');
+    Route::post('new-supplier', [SupplierController::class, 'saveSupplierProduct'])->name('new-supplier');
 
 // });
